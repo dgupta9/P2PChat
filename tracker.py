978a1a760dd8af7eaae143e86ea2fc207897263e
HAOYU LI <HAOYU LI>
2016-11-17 19:45:43 -0500

    modified by Haoyu

--- tracker.py
+++ tracker.py
@@ -1,4 +1,5 @@
 import json
+import pickle
 from socket import *
 serverPort = 9990
 serverSocket = socket(AF_INET, SOCK_DGRAM)
@@ -18,6 +19,7 @@ def checkactions(action, clname,clip):
 	if action==0:
 		if clname in namelist:              #name conflict, sendbackcontrol=2
 			sendbackcontrol=2
+
 		elif len(namelist)>=5:              #more than 5 clients exist, need to check other trackers. sendbackcontrol=3
 			sendbackcontrol=3
 		else:
@@ -26,29 +28,37 @@ def checkactions(action, clname,clip):
 			sendbackcontrol=1
 
 
+def response(a):
+	if a == 1:
+		responsemessage = [messagelist[0], 0, 0, messagelist[3], messagelist[4]]
+		response = pickle.dumps(responsemessage)
+		serverSocket.sendto(response.encode(), clientAddress)
+		print "The user has registered successfully"
+	elif a== 2:
+		responsemessage = [messagelist[0], 128, 0, messagelist[3], messagelist[4]]            #if name confliction,
+		response = pickle.dumps(responsemessage)
+		serverSocket.sendto(response.encode(), clientAddress)
+		print "User name confliction"
+
 
 
 
 
 
 while True:
-	message, clientAddress = serverSocket.recvfrom(2048)
-	messagelist=json.loads(message)
+	m1, clientAddress = serverSocket.recvfrom(2048)
+	#message=message.decode()
+	messagelist=pickle.loads(m1)
 	print "Received "
 	print messagelist
+
+	checkactions(messagelist[2],messagelist[4],messagelist[3])
+	response(sendbackcontrol)
 	print "name"
 	print namelist
 	print "ip"
 	print iplist
-	checkactions(messagelist[2],messagelist[4],messagelist[3])
-	if sendbackcontrol==1:
-		responsemessage=[messagelist[0],0,0,messagelist[3],messagelist[4]]
-		response = json.dumps(responsemessage)
-		serverSocket.sendto(response, clientAddress)
-	elif sendbackcontrol==2:
-		responsemessage = [messagelist[0], 128, 0, messagelist[3], messagelist[4]]
-		response = json.dumps(responsemessage)
-		serverSocket.sendto(response, clientAddress)
+	print len(namelist),"users registered in this tracker"
 
 	continue
 
